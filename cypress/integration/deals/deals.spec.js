0bc48cb5e40d1790e6f73126dd82c4d49ad34ff5
bethmenzies <bethmenzies@btinternet.com>
2021-06-05 19:32:10 +0100

    clean up of deals test

--- cypress/integration/deals/deals.spec.js
+++ cypress/integration/deals/deals.spec.js
@@ -1,4 +1,4 @@
-/** Feature: This feature will make sure that the shop page is navigable and usable.
+/* Feature: This feature will make sure that the shop page is navigable and usable.
 Scenario: User navigates to shop page
 Given I am on the home page
 When I navigate to ‘Deals’
@@ -7,13 +7,6 @@ Then the user should be on the https://www.sky.com/deals page
 Note: You will have to handle the ‘accept cookies’ in your test.
 Home page is https://www.sky.com/
 It is better to clear the cookies for sky.com or clear your web page history.
-
-
-Scenario: User sees tiles on the shop page
-Given I am on the home page
-When I try to sign in with invalid credentials 
-Then I should see a text saying that ‘Sorry, we did not recognise either your username or password’
-(See Snapshot A)
 
 
 Scenario: User sees a list of deals on the deals page 
@@ -25,7 +18,7 @@ For example:
 Note: Please change the offer prices if the page shows something different. Assert only the first 3 offers.
 */
 
-describe('The shop page is navigable and usable', () => {
+describe('The deals page is navigable and usable', () => {
     before (() => {
         cy.fixture('acceptCookieNotice').then((json) => {
             cy.intercept('GET', 'wrapper/tcfv2/v1/gdpr/native-message*', json)
@@ -47,11 +40,11 @@ describe('The shop page is navigable and usable', () => {
         cy.contains('Deals').click()
 
         // then I should be on the https://www.sky.com/deals page 
-        // given I am on the deals page
         cy.location('pathname').should('eq', '/deals')
 
         cy.contains('Sky Deals').should('exist').should('be.visible')
 
+        // given I am on the deals page
         // then I see a list of deals with prices
         cy.get('section.box__Box-eb0ezq-0').within(() => {
             cy.get('.box__Box-eb0ezq-0.jOIfJV').should('have.length', 6)
